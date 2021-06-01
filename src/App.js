78e4b7821a73fd1da8ae156c8a224b8461872d6b
Sameep Sheth <sameepsheth@gmail.com>
2021-06-01 18:58:54 -0400

    pipeline title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const dar = "blah";
   return (
     <div className="App">
+      <h1>Pipeline</h1>
       <h1>The Env is :</h1>
       <h1>{process.env.REACT_APP_KEY}</h1>
     </div>
